1dac8f53468e51a086df112a5e3231d17c177813
Aleksey Grigorchuk <alexgrig2222@yandex.ru>
2023-02-06 21:56:32 +0400

    Исправили поведение кнопки. Не в правильном порядке использовали методы жизненного цикла.

--- app/src/main/java/com/example/moodcalendar4/AddRecordFragment.java
+++ app/src/main/java/com/example/moodcalendar4/AddRecordFragment.java
@@ -12,26 +12,27 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class AddRecordFragment extends DialogFragment {
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-        LayoutInflater inflater = requireActivity().getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.dialog_create_record, null);
-
-        builder.setView(dialogView);
-        return builder.create();
-    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = requireView();
-        root.findViewById(R.id.button_cancel).setOnClickListener(
+        View dialogView = inflater.inflate(R.layout.dialog_create_record, null);
+        dialogView.findViewById(R.id.button_cancel).setOnClickListener(
                 button -> dismiss()
         );
-        return root;
+        return dialogView;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//
+//        LayoutInflater inflater = requireActivity().getLayoutInflater();
+//        View dialogView = inflater.inflate(R.layout.dialog_create_record, null);
+//
+//        builder.setView(dialogView);
+        return super.onCreateDialog(savedInstanceState);
     }
 
     public static String TAG = "AddRecordFragment";
